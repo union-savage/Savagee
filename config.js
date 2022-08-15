a634fe38b6460a346847361a4fb1be85809ab61c
Elainabot1/Lordsavage <102780931+union-savage@users.noreply.github.com>
2022-08-16 00:24:59 +0200

    Update config.js
    Signed-off-by: Elainabot1/Lordsavage <102780931+union-savage@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,13 +37,13 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
+global.owner = ['263716564623','']
+global.premium = ['263716564623','263787825468']
+global.botname = 'Elaina BOT INC'
 global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
+global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4ecA1A'
+global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINwSWoA'
+global.packname = 'Lord-Savage BOT INC'
 global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
 global.monayawal = '100000'
 global.darahawal = '100'
